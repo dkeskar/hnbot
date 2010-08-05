dfcf339b0eab9668b845e87572bedc9b1726030e
Dhananjay Keskar <dkeskar@memamsa.com>
2010-08-05 16:37:16 -0700

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,32 @@
 set :application, "watchbots"
+set :user, "app"
+set :deploy_to, "/home/#{user}/#{application}"
+set :scm, :git
+default_run_options[:pty] = true
 set :repository,  "git@track.memamsa.net:watchbots.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+set :use_sudo, false
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :app, "nyx.memamsa.com"
+role :web, "nyx.memamsa.com"
+role :db,  "nyx.memamsa.com", :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+after "deploy:symlink", "deploy:update_crontab"
 
-# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  
+  desc "Update the crontab file"
+  task :whenever, :roles => :db do
+    run "cd #{release_path} && whenever --set 'environment=production&cron_log=#{shared_path}/log/cron.txt' && whenever --update-crontab #{application}"  
+  end
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+end
